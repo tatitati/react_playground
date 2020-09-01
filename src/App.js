ee3505e5fbae92f6d55a4e343c2169e005f1c556
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-02 00:05:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+     super();
+     this.state = {
+        data: 
+        [
+           {
+              "id":1,
+              "name":"Foo",
+              "age":"20"
+           },
+           {
+              "id":2,
+              "name":"Bar",
+              "age":"30"
+           },
+           {
+              "id":3,
+              "name":"Baz",
+              "age":"40"
+           }
+        ]
+     }
+  }
+  render() {
+     return (
+        <div>
+           <Header/>
+           <table>
+              <tbody>
+                {
+                  this.state.data.map(
+                    (person, i) => <TableRow key = {i} data = {person} />
+                  )
+                }
+              </tbody>
+           </table>
+        </div>
+     );
+  }
 }
-
+class Header extends React.Component {
+  render() {
+     return (
+        <div>
+           <h1>Header</h1>
+        </div>
+     );
+  }
+}
+class TableRow extends React.Component {
+  render() {
+     return (
+        <tr>           
+           <td>Id: {this.props.data.id}</td>
+           <td>Name: {this.props.data.name}</td>
+           <td>Age: {this.props.data.age}</td>
+        </tr>
+     );
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,49 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};  
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(      
+        () => this.tick(),
+        1000    
+      );  
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);  
+  }
+
+  tick() {    
+    this.setState({      
+      date: new Date()    
+    });  
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function tick() {
+  ReactDOM.render(
+      <Clock />,        
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
