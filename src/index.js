a43cb985cbebb125d9ec66bc0467fab1780716fb
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-07 21:35:05 +0100

    simplifgyin

--- src/index.js
+++ src/index.js
@@ -31,24 +31,14 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>This is the code in index.js</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
-function tick() {
-  ReactDOM.render(
-      <Clock />,        
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Clock />,        
+  document.getElementById('root')
+);
