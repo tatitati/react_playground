6a4b6e11550dd83ccafee0df7d52fe685eb72c37
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-16 21:30:30 +0100

    added some LoginControl button, to show how conditional render works

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Clock from './myfiles/clock.js';
 import Toggle from './myfiles/toggle.js';
+import LoginControl from './myfiles/LoginControl.js';
+import Greeting from './myfiles/Greeting.js';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
+// "yarn start" render this file
 
 ReactDOM.render(
     [
       <h1> From index.js </h1>,
-      <h3> From clock.js </h3>,
-      <Clock />, 
-      <h3> From toggle.js </h3>,
-      <Toggle />
+          <h3>    From clock.js </h3>,
+                <Clock />, 
+          <h3>    From toggle.js </h3>,
+                <Toggle />,
+          <h3>    From LoginControl.js </h3>,
+                <LoginControl />,                
     ],
   document.getElementById('root')
 );
+
